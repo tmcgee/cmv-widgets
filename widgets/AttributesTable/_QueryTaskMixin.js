cab419113a502d6cbea08d1bf66e723cbf265cb3
Gregg Roemhildt <roemhildtg@gmail.com>
2016-12-15 10:57:54 -0600

    use the value of layer definitions if its defined

--- widgets/AttributesTable/_QueryTaskMixin.js
+++ widgets/AttributesTable/_QueryTaskMixin.js
@@ -484,7 +484,7 @@ define([
 
                         url = layer.url;
                     } else if (layer.declaredClass === 'esri.layers.ArcGISDynamicMapServiceLayer') { // Dynamic Layer
-                        whereByLayerDef = layer.layerDefinitions && layer.layerDefinitions[qp.sublayerID];
+                        whereByLayerDef = layer.layerDefinitions ? layer.layerDefinitions[qp.sublayerID] : undefined;
 
 
                         if (qp.sublayerID !== null) {
