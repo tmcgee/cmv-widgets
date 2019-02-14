e2f34af4fefdc1469cf9434a4bfef0b14895353a
ERS-Long <long.d.ma@gmail.com>
2019-02-14 15:17:04 -0600

    Update MapLoading.js
    Added a text 'Loading' next to the spin icon

--- widgets/MapLoading.js
+++ widgets/MapLoading.js
@@ -25,7 +25,8 @@ define([
 
         className: 'fa fa-spinner fa-spin',
         style: 'color:#333;text-shadow:2px 2px #eee;font-size:32px;display:none;position:absolute;top:calc(50% - 16px);left:calc(50% - 16px);z-index:999',
-
+        text_style: 'color:#333;text-shadow:2px 2px #eee;font-size:32px;display:none;position:absolute;top:calc(50% - 16px);left:calc(50% + 20px);z-index:999',
+        
         postCreate: function () {
             this.inherited(arguments);
 
@@ -33,7 +34,11 @@ define([
                 className: this.className,
                 style: this.style
             });
-
+            this.loadingText = put(this.map.root, 'i', {
+                className: '',
+                style: this.text_style,
+                textContent: 'Loading'
+            });            
             on(this.map, 'update-start', lang.hitch(this, 'showLoading'));
             on(this.map, 'update-end', lang.hitch(this, 'hideLoading'));
 
@@ -43,12 +48,14 @@ define([
 
         showLoading: function () {
             domStyle.set(this.loading, 'display', 'block');
+            domStyle.set(this.loadingText, 'display', 'block');
             this.map.disableMapNavigation();
             this.map.hideZoomSlider();
         },
 
         hideLoading: function () {
             domStyle.set(this.loading, 'display', 'none');
+            domStyle.set(this.loadingText, 'display', 'none');            
             this.map.enableMapNavigation();
             this.map.showZoomSlider();
         }
