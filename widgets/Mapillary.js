0a826bd644a573ab22c1e20cb92f6c521db2b389
Tim McGee <github@mmconsult.com>
2019-11-26 00:15:14 -0800

    no need for the slash

--- widgets/Mapillary.js
+++ widgets/Mapillary.js
@@ -113,7 +113,7 @@ define([
             text: '\uf124',
             font: {
                 size: 16,
-                family: 'Font Awesome\ 5 Free'
+                family: 'Font Awesome 5 Free'
             }
         },
 
